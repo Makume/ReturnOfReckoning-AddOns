b942869167133c00a662b34a22482a9039a6cced
Matthias Kühl <72539570+Makume@users.noreply.github.com>
2024-05-09 15:48:26 +0200

    Version 1.0.0

---
+++ RezEmote/README.md
@@ -0,0 +1,8 @@
+# RezEmote v1.0.0
+
+The AddOn RezEmote send the Rez emote on player death.
+This AddOn help to trigger the AddOn [ResHelp](https://tools.idrinth.de/addons/reshelp/)
+
+# Chat Commands
+
+There are not chat commands
---
+++ RezEmote/RezEmote.lua
@@ -0,0 +1,29 @@
+if not RezEmote then 
+	RezEmote = {} 
+end
+
+function RezEmote.Initialize()
+	RegisterEventHandler(SystemData.Events.PLAYER_DEATH, "RezEmote.Rez")
+end
+
+function RezEmote.Rez()
+	if GameData.Player.rvrZoneFlagged and (not RezEmote.isScenario()) then
+		EA_ChatWindow.Print(GameData.Player.name..towstring(L" request a rez"), SystemData.ChatLogFilters.EMOTE)
+	end 
+end
+
+function RezEmote.isScenario()
+    if GameData.Player.isInScenario then
+    	return true
+    end
+    if GameData.Player.isInSiege then
+    	return true
+    end
+    if GameData.Player.zone == 167 then -- IC Siege
+    	return true
+    end
+    if GameData.Player.zone == 168 then -- Altdorf Siege
+    	return true
+    end
+    return false
+end
