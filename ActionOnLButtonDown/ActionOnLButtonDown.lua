cdc21a4d4250fe12e31e2269c752c7c50fa26de2
Matthias Kühl <72539570+Makume@users.noreply.github.com>
2024-05-22 17:23:35 +0200

    Version 1.0.0

---
+++ ActionOnLButtonDown/ActionOnLButtonDown.lua
@@ -0,0 +1,8 @@
+if not ActionOnLButtonDown then 
+	ActionOnLButtonDown = {} 
+end
+
+function ActionOnLButtonDown.OnInitialize()
+    ActionButton.OnLButtonDown = ActionButton.OnLButtonUp
+    ActionButton.OnLButtonUp = nil
+end
---
+++ DisableCombatLog/DisableCombatLog.lua
@@ -0,0 +1,7 @@
+if not DisableCombatLog then 
+	DisableCombatLog = {} 
+end
+
+function DisableCombatLog.OnInitialize()
+    TextLogSetEnabled ("Combat", false)    
+end
