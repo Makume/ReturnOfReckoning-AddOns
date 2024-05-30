1dbdd1f2f5af9fec59b95c75948b2f0d1f87b404
Matthias Kühl <72539570+Makume@users.noreply.github.com>
2024-05-30 19:48:28 +0200

    Version 2.8.6

--- Enemy/Code/Marks/MarksEditTemplate.lua
+++ Enemy/Code/Marks/MarksEditTemplate.lua
@@ -2,8 +2,9 @@ if not Enemy.MarksEditListData then
 	Enemy.MarksEditListData = {} 
 end
 
-local _index = 0
+local Index = 0
 local ConfigData = {}
+local PopulateData = {}
 local WindowName = "EnemyMarkEntryDialog"
 local AddWindowName = "AddMarkPlayer"
 local EditWindowName = "EditMarkPlayer"
@@ -13,7 +14,7 @@ function Enemy.MarksUI_MarkEditEntryDialog_Open(template, onOkCallback)
 		DestroyWindow(WindowName)
 	end
 	CreateWindow(WindowName, false)
-	LabelSetText(WindowName.."TitleBarText", L"Edit mark entries ("..towstring(template.name)..L")")
+	LabelSetText(WindowName.."TitleBarText", L"Edit mark entries ("..template.name..L")")
 	ButtonSetText(WindowName.."AddButton", L"Add Entry")
 	ButtonSetText(WindowName.."OkButton", L"OK")
 	ButtonSetText(WindowName.."CancelButton", L"Cancel")
@@ -24,10 +25,10 @@ function Enemy.MarksUI_MarkEditEntryDialog_Open(template, onOkCallback)
 end
 
 function Enemy.OnLButtonUpMarkEntry()	
-	_index = ListBoxGetDataIndex(WindowName.."List", WindowGetId(SystemData.MouseOverWindow.name))
-	if _index ~= nil then
+	Index = ListBoxGetDataIndex(WindowName.."List", WindowGetId(SystemData.MouseOverWindow.name))
+	if Index ~= nil then
 		Enemy.ColorMarkEntry()
-		EA_Window_ContextMenu.CreateContextMenu(SystemData.MouseOverWindow.name, EA_Window_ContextMenu.CONTEXT_MENU_1, towstring(Enemy.MarksEditListData[_index].name))
+		EA_Window_ContextMenu.CreateContextMenu(SystemData.MouseOverWindow.name, EA_Window_ContextMenu.CONTEXT_MENU_1, towstring(Enemy.MarksEditListData[Index].name))
 		EA_Window_ContextMenu.AddMenuDivider()
 		EA_Window_ContextMenu.AddMenuItem(L"Edit", Enemy.MarksUI_MarkEditEntryDialog_Edit, false, true, EA_Window_ContextMenu.CONTEXT_MENU_1)
 		EA_Window_ContextMenu.AddMenuItem(L"Delete", Enemy.MarksUI_MarkEditEntryDialog_Delete, false, true, EA_Window_ContextMenu.CONTEXT_MENU_1)
@@ -39,20 +40,25 @@ function Enemy.OnMouseOverMarkEntry()
 	Enemy.ColorMarkEntry()
 end
 
+function Enemy.MarksEditListData_OnEffectFiltersListPopulate()
+	if not EnemyMarkEntryDialogList.PopulatorIndices then 
+		return 
+	end
+	PopulateData = EnemyMarkEntryDialogList.PopulatorIndices
+end
+
 function Enemy.ColorMarkEntry()
 	ColorIndex = ListBoxGetDataIndex(WindowName.."List", WindowGetId(SystemData.MouseOverWindow.name))
-	local i = 1
 	local row_window_name = L""
-	for k,v in pairs(ConfigData.permanentTargets) do
-		row_window_name = towstring(WindowName)..L"ListRow"..towstring(i)
-		if (i == ColorIndex) then
+	for k, v in ipairs(PopulateData) do
+		row_window_name = towstring(WindowName)..L"ListRow"..towstring(k)
+		if PopulateData[k] == ColorIndex then
 			WindowSetShowing(tostring(row_window_name..L"Background"), true)
 			WindowSetAlpha(tostring(row_window_name..L"Background"), 0.5)
-			WindowSetTintColor(tostring(row_window_name..L"Background"), 200, 200, 100)			
+			WindowSetTintColor(tostring(row_window_name..L"Background"), 200, 200, 100)
 		else
 			WindowSetShowing(tostring(row_window_name..L"Background"), false)
 		end
-		i = i + 1
 	end
 end
 
@@ -67,12 +73,12 @@ function Enemy.MarksUI_MarkEditEntryDialog_Edit()
 	if (ButtonGetDisabledFlag(SystemData.ActiveWindow.name) == true) then
 		return
 	end
-	if (_index == 0) then 
+	if (Index == 0) then 
 		return
 	end
 	CreateWindow(EditWindowName, false)
     LabelSetText(EditWindowName.."Label", L"Edit Playername")
-	TextEditBoxSetText(EditWindowName.."Text", towstring(Enemy.MarksEditListData[_index].name))
+	TextEditBoxSetText(EditWindowName.."Text", Enemy.MarksEditListData[Index].name)
     ButtonSetText(EditWindowName.."OKButton", L"OK")
     ButtonSetText(EditWindowName.."CancelButton", L"Cancel")
 	WindowSetShowing(EditWindowName, true)
@@ -82,7 +88,7 @@ function Enemy.MarksUI_MarkEditEntryDialog_Delete()
 	if (ButtonGetDisabledFlag(SystemData.ActiveWindow.name) == true) then
 		return
 	end
-	if (_index == 0) then 
+	if (Index == 0) then 
 		return
 	end
 	ConfigData.permanentTargets = Enemy.clone(Enemy.MakePermanentTargetsCopy())
@@ -116,7 +122,9 @@ function Enemy.InitMarksEditListData()
 			table.insert(_DisplayOrder, i)
 			table.insert(Enemy.MarksEditListData, { name = towstring(k)})
 			row_window_name = towstring(WindowName)..L"ListRow"..towstring(i)
-			WindowSetShowing(tostring(row_window_name..L"Background"), false)		
+			if (DoesWindowExist(tostring(row_window_name))) then
+				WindowSetShowing(tostring(row_window_name..L"Background"), false)
+			end
 			i = i + 1
 		end
 	end
@@ -130,30 +138,30 @@ end
 
 function Enemy.OnAddMarkPlayerAccept()
 	local NewPlayerName = TextEditBoxGetText(AddWindowName.."Text")
-    if (NewPlayerName == nil) then
-        return
-    end
+	if (NewPlayerName == nil) then
+		return
+	end
 	Enemy.OnCancelAddMarkPlayer()
-	ConfigData.permanentTargets[towstring(NewPlayerName)] = false   
+	ConfigData.permanentTargets[towstring(NewPlayerName)] = false
 	Enemy.InitMarksEditListData()
 end
 
 function Enemy.OnCancelAddMarkPlayer()
-    WindowSetShowing(AddWindowName, false)
-    TextEditBoxSetText(AddWindowName.."Text", L"")       
+	WindowSetShowing(AddWindowName, false)
+	TextEditBoxSetText(AddWindowName.."Text", L"")
 end
 
 function Enemy.OnCancelEditMarkPlayer()
     WindowSetShowing(EditWindowName, false)
-    TextEditBoxSetText(EditWindowName.."Text", L"")        
+    TextEditBoxSetText(EditWindowName.."Text", L"")
 end
 
 function Enemy.OnEditMarkPlayerAccept()
 	local NewPlayerName = TextEditBoxGetText(EditWindowName.."Text")
-    if (NewPlayerName == nil) then
-        return
-    end
-	if (NewPlayerName == towstring(Enemy.MarksEditListData[_index].name)) then
+	if (NewPlayerName == nil) then
+		return
+	end
+	if (NewPlayerName == towstring(Enemy.MarksEditListData[Index].name)) then
 		return
 	end
     Enemy.OnCancelEditMarkPlayer()
@@ -165,7 +173,7 @@ end
 function Enemy.MakePermanentTargetsCopy()
 	local permanentTargetsCopy = {}
 	for k,v in pairs(ConfigData.permanentTargets) do
-		if (towstring(k) ~= towstring(Enemy.MarksEditListData[_index].name)) then
+		if (towstring(k) ~= towstring(Enemy.MarksEditListData[Index].name)) then
 			permanentTargetsCopy[towstring(k)] = false
 		end	
 	end
