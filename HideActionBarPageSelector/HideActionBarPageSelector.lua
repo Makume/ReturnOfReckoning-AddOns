5ae1c7eb2f7b08134d8cc5e1cbdbe3f3d4af9f96
Matthias Kühl <info@makume.de>
2024-09-28 12:08:37 +0200

    Delete

--- HideActionBarPageSelector/HideActionBarPageSelector.lua
+++
@@ -1,8 +0,0 @@
-if not HideActionBarPageSelector then 
-	HideActionBarPageSelector = {} 
-end
-
-function HideActionBarPageSelector.Initialize()
-	ActionBarPageSelector.Create = function(self, ...)
-	end
-end
